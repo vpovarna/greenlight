213f1c8725cd4519c489d1e51536d2d951f06453
povarna <vlad.povarna@uipath.com>
2024-02-27 17:32:02 +0200

    Adding json support

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	js := `{"status": "available", "environment": %q, "version": %q}`
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	js = fmt.Sprintf(js, app.config.env, version)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "Server error", http.StatusInternalServerError)
+	}
 }
