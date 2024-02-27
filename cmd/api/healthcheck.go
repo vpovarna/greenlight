0fd70cdb5a4f6d7d9b05c3e1f858e2d4beeef0e8
povarna <vlad.povarna@uipath.com>
2024-02-27 13:17:27 +0200

    Adding movies routes and json format to healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,8 +5,12 @@ import (
 	"net/http"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+
+	js := `{"status": "available", "environment": %q, "version": %q}`
+
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -38,14 +38,10 @@ func main() {
 		logger: logger,
 	}
 
-	// Create a mutex server & handlers
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	// Create HTTP server
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
