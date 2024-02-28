815a0acf41376365f912c113c217873f853d7c5e
povarna <vlad.povarna@uipath.com>
2024-02-28 21:47:26 +0200

    Adding custom Unmarshaler for runtime field

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +15,30 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJsonValue := strconv.Quote(jsonValue)
 	return []byte(quotedJsonValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	unquotedJson, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidFormat
+	}
+
+	parts := strings.Split(unquotedJson, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidFormat
+	}
+
+	// Convert the int32 to a Runtime type and assign this to the receiver. Note that we
+	// use the * operator to deference the receiver (which is a pointer to a Runtime
+	// type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
 }
