aa18ec699b19b1ee76c2b29206caddc137ac9c0e
povarna <vlad.povarna@uipath.com>
2024-03-15 12:47:26 +0200

    Adding a background go routine to catch the close signal.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,6 +17,23 @@ func (app *application) serve() error {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// This blocks until we receive a SIGINT and SIGTERM
+		s := <-quit
+
+		// Log the caught signal
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		// Exit the application with a 0 (success) status code
+		os.Exit(0)
+	}()
 
 	app.logger.PrintInfo("Starting server", map[string]string{
 		"addr": srv.Addr,
